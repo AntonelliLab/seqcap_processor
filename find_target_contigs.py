070d3f2ed4d142ab50af46c20524dc258e17daa0
Tobias Hofmann <tobiashofmann@gmx.net>
2015-10-08 17:09:19 +0200

    Update find_target_contigs.py

--- find_target_contigs.py
+++ find_target_contigs.py
@@ -1,10 +1,42 @@
 #!/usr/local/anaconda/bin/python
 # encoding: utf-8
 
-#Created by Brant Faircloth on 02 June 2011.
-#Copyright 2011 Brant C. Faircloth. All rights reserved.
+"""
+Copyright (c) 2010-2012, Brant C. Faircloth All rights reserved.
 
-#Modified by Tobias Hofmann (tobiashofmann@gmx.net) for application for other than UCE datasets.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+* Neither the name of the University of California, Los Angeles nor the names
+of its contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+# Modified by Tobias Hofmann (tobiashofmann@gmx.net):
+# Modifications include: 	- removal of unnecessary flag-options
+# 				- renaming of uce-related parameters into exon-terminology
+#				- modification of regex-patterns to match Trinity-contigs and palm-exon set probe-order file
+# Additions include:		- automatic generation of config file including all recovered exon names for further processing in this pipeline
+# 				- automatic generation of the match-table in tab-delimited text-format to be opened in e.g. Excel for match overview
+# 				- user-input choice for path to sqlite3, which is necesarry to access the database and generate the match-text-file
 
 
 import re
