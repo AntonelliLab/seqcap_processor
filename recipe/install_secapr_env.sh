f0f0889d59da96495c8eb5b356a1df4d4a8debd4
Tobias Andermann <tobias.andermann@bioenv.gu.se>
2021-07-18 13:58:19 +0200

    updated installation script

--- recipe/install_secapr_env.sh
+++ recipe/install_secapr_env.sh
@@ -7,7 +7,7 @@ source activate secapr_env
 sleep 5
 conda install -y pandas
 conda install -y matplotlib-base
-conda install -y biopython==1.76
+conda install -y biopython
 conda install -y trimmomatic
 conda install -y fastqc
 conda install -y abyss
