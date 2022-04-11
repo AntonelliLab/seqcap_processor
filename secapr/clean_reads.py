d729f215e555404d7c7213d5c172677002ebdafb
Tobias Andermann <tobias.andermann@bioenv.gu.se>
2022-04-11 12:12:59 +0200

    fixed longest common substring function

--- secapr/clean_reads.py
+++ secapr/clean_reads.py
@@ -124,18 +124,27 @@ def add_arguments(parser):
 
 
 def longest_common_substring(s1, s2):
-    m = [[0] * (1 + len(s2)) for i in range(1 + len(s1))]
-    longest, x_longest = 0, 0
-    for x in range(1, 1 + len(s1)):
-        for y in range(1, 1 + len(s2)):
-            if s1[x - 1] == s2[y - 1]:
-                m[x][y] = m[x - 1][y - 1] + 1
-                if m[x][y] > longest:
-                    longest = m[x][y]
-                    x_longest = x
-            else:
-                m[x][y] = 0
-    return s1[x_longest - longest: x_longest]
+    m = []
+    for i, letter in enumerate(list(s1)):
+        if letter == list(s2)[i]:
+            m.append(letter)
+        else:
+            break
+    return ''.join(m)
+
+# def longest_common_substring(s1, s2):
+#     m = [[0] * (1 + len(s2)) for i in range(1 + len(s1))]
+#     longest, x_longest = 0, 0
+#     for x in range(1, 1 + len(s1)):
+#         for y in range(1, 1 + len(s2)):
+#             if s1[x - 1] == s2[y - 1]:
+#                 m[x][y] = m[x - 1][y - 1] + 1
+#                 if m[x][y] > longest:
+#                     longest = m[x][y]
+#                     x_longest = x
+#             else:
+#                 m[x][y] = 0
+#     return s1[x_longest - longest: x_longest]
 
 def read_count(input):
     # Use bash command to count the read number
